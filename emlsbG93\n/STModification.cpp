a2ee66c7d63cb3ff6114da3c0a7cf223fc189e2f
Shawn Zhang <zsx19931129@163.com>
2016-11-22 08:41:44 +0800

    recursive update

--- emlsbG93\n/STModification.cpp
+++ emlsbG93\n/STModification.cpp
@@ -53,88 +53,90 @@ void Tree::insert(int val){
     }
 }
 
-//Node* deleteNodeRecursive(Node* rt, int val){
-//    if(rt == NULL){
-//        return rt;
-//    }
-//    if(rt->val < val){
-//        rt->left = deleteNodeRecursive(rt->left, val);
-//    }else if(rt->val > val){
-//        rt->right = deleteNodeRecursive(rt->right, val);
-//    }else{
-//        if(rt->mid){
-//            rt->mid = deleteNodeRecursive(rt->mid, val);
-//        }else if(rt->right){
-//            Node *iter = rt->right;
-//            while(iter->left){
-//                iter = iter->left;
-//            }
-//            int minval = iter->val;
-//            rt->val = minval;
-//            rt->mid = iter->mid;
-//            iter->mid = NULL;
-//            rt->right = deleteNodeRecursive(rt->right, minval);
-//        }else{
-//            rt = rt->left;
-//        }
-//    }
-//    return rt;
-//}
-
-// 1.recursive
-//void Tree::remove(int val){
-//    root = deleteNodeRecursive(root, val);
-//}
-
-// 2.iterate
-void Tree::remove(int val){
-    Node *cur = root, *parent = NULL, *del=NULL;
-    while(cur){
-        if(val > cur->val){
-            parent = cur;
-            cur = cur->right;
-        }else if(val < cur->val){
-            parent = cur;
-            cur = cur->left;
+Node* deleteNodeRecursive(Node* rt, int val){
+    if(rt == NULL){
+        return rt;
+    }
+    if(rt->val > val){
+        rt->left = deleteNodeRecursive(rt->left, val);
+    }else if(rt->val < val){
+        rt->right = deleteNodeRecursive(rt->right, val);
+    }else{
+        if(rt->mid){
+            rt->mid = deleteNodeRecursive(rt->mid, val);
+        }else if(rt->right){
+            Node *iter = rt->right;
+            while(iter->left){
+                iter = iter->left;
+            }
+            int minval = iter->val;
+            rt->val = minval;
+            rt->mid = iter->mid;
+            iter->mid = NULL;
+            rt->right = deleteNodeRecursive(rt->right, minval);
         }else{
-            del = cur;
-            Node *newSubRoot = NULL;
-            bool last = false;
-            if(cur->mid){
-                newSubRoot = cur->mid;
-            }else if(cur->right){
-                Node *iter = cur->right, *subParent = cur;
-                while(iter->left){
-                    subParent = iter;
-                    iter = iter->left;
-                }
-                newSubRoot = iter;
-                if(subParent->val > iter->val){
-                    subParent->left = iter->right;
-                }else{
-                    subParent->right = iter->right;
-                }
-            }else{
-                last = true;
-                newSubRoot = cur->left;
-            }
-            if(!parent){
-                root = newSubRoot;
-            }else{
-                if(parent->val > cur->val){
-                    parent->left = newSubRoot;
-                }else{
-                    parent->right = newSubRoot;
-                }
-            }
-            if(!last){
-                newSubRoot->left = cur->left;
-                newSubRoot->right = cur->right;
-            }
-            break;
+            Node *del = rt;
+            rt = rt->left;
+            delete del;
         }
     }
-    if(del){
-        delete del;
-    }
+    return rt;
 }
+
+// 1.recursive
+void Tree::remove(int val){
+    root = deleteNodeRecursive(root, val);
+}
+
+// 2.iterate
+//void Tree::remove(int val){
+//    Node *cur = root, *parent = NULL, *del=NULL;
+//    while(cur){
+//        if(val > cur->val){
+//            parent = cur;
+//            cur = cur->right;
+//        }else if(val < cur->val){
+//            parent = cur;
+//            cur = cur->left;
+//        }else{
+//            del = cur;
+//            Node *newSubRoot = NULL;
+//            bool last = false;
+//            if(cur->mid){
+//                newSubRoot = cur->mid;
+//            }else if(cur->right){
+//                Node *iter = cur->right, *subParent = cur;
+//                while(iter->left){
+//                    subParent = iter;
+//                    iter = iter->left;
+//                }
+//                newSubRoot = iter;
+//                if(subParent->val > iter->val){
+//                    subParent->left = iter->right;
+//                }else{
+//                    subParent->right = iter->right;
+//                }
+//            }else{
+//                last = true;
+//                newSubRoot = cur->left;
+//            }
+//            if(!parent){
+//                root = newSubRoot;
+//            }else{
+//                if(parent->val > cur->val){
+//                    parent->left = newSubRoot;
+//                }else{
+//                    parent->right = newSubRoot;
+//                }
+//            }
+//            if(!last){
+//                newSubRoot->left = cur->left;
+//                newSubRoot->right = cur->right;
+//            }
+//            break;
+//        }
+//    }
+//    if(del){
+//        delete del;
+//    }
+//}
